1785df46325b2e4bd373cbe1b7ff45e02008828f
HamidRazi <106916161+HamidRazi@users.noreply.github.com>
2022-06-05 21:18:46 +0430

    Add files via upload

---
+++ js/bandit-runsheet.js
@@ -0,0 +1,103 @@
+// specify stimuli
+var face_conditioning_stimuli = [
+
+  // free choice trials
+  { choice_type: "free", left_image_type: "face", left_image_number: 0, right_image_type: "face", right_image_number: 1 },
+  { choice_type: "free", left_image_type: "face", left_image_number: 1, right_image_type: "face", right_image_number: 0 },
+  { choice_type: "free", left_image_type: "face", left_image_number: 0, right_image_type: "face", right_image_number: 1 },
+  { choice_type: "free", left_image_type: "face", left_image_number: 1, right_image_type: "face", right_image_number: 0 },
+  { choice_type: "free", left_image_type: "face", left_image_number: 0, right_image_type: "face", right_image_number: 1 },
+  { choice_type: "free", left_image_type: "face", left_image_number: 1, right_image_type: "face", right_image_number: 0 },
+
+  // forced choice trials
+  { choice_type: "forced", left_image_type: "face", left_image_number: null, right_image_type: null, right_image_number: null },
+  { choice_type: "forced", left_image_type: null, left_image_number: null, right_image_type: "face", right_image_number: null },
+  { choice_type: "forced", left_image_type: "face", left_image_number: null, right_image_type: null, right_image_number: null },
+  { choice_type: "forced", left_image_type: null, left_image_number: null, right_image_type: "face", right_image_number: null }
+
+]
+
+// specify stimuli
+var place_conditioning_stimuli = [
+
+    // free choice trials
+    { choice_type: "free", left_image_type: "place", left_image_number: 0, right_image_type: "place", right_image_number: 1 },
+    { choice_type: "free", left_image_type: "place", left_image_number: 1, right_image_type: "place", right_image_number: 0 },
+    { choice_type: "free", left_image_type: "place", left_image_number: 0, right_image_type: "place", right_image_number: 1 },
+    { choice_type: "free", left_image_type: "place", left_image_number: 1, right_image_type: "place", right_image_number: 0 },
+    { choice_type: "free", left_image_type: "place", left_image_number: 0, right_image_type: "place", right_image_number: 1 },
+    { choice_type: "free", left_image_type: "place", left_image_number: 1, right_image_type: "place", right_image_number: 0 },
+
+    // forced choice trials
+    { choice_type: "forced", left_image_type: "place", left_image_number: null, right_image_type: null, right_image_number: null },
+    { choice_type: "forced", left_image_type: null, left_image_number: null, right_image_type: "place", right_image_number: null },
+    { choice_type: "forced", left_image_type: "place", left_image_number: null, right_image_type: null, right_image_number: null },
+    { choice_type: "forced", left_image_type: null, left_image_number: null, right_image_type: "place", right_image_number: null }
+
+]
+
+
+// specify stimuli
+var bugmush_conditioning_stimuli = [
+
+  // free choice trials
+  { choice_type: "free", left_image_type: "bugmush", left_image_number: 0, right_image_type: "bugmush", right_image_number: 1 },
+  { choice_type: "free", left_image_type: "bugmush", left_image_number: 1, right_image_type: "bugmush", right_image_number: 0 },
+  { choice_type: "free", left_image_type: "bugmush", left_image_number: 0, right_image_type: "bugmush", right_image_number: 1 },
+  { choice_type: "free", left_image_type: "bugmush", left_image_number: 1, right_image_type: "bugmush", right_image_number: 0 },
+  { choice_type: "free", left_image_type: "bugmush", left_image_number: 0, right_image_type: "bugmush", right_image_number: 1 },
+  { choice_type: "free", left_image_type: "bugmush", left_image_number: 1, right_image_type: "bugmush", right_image_number: 0 },
+
+]
+
+// specify stimuli
+var infosource_conditioning_stimuli = [
+
+    // free choice trials
+    { choice_type: "free", left_image_type: "infosource", left_image_number: 0, right_image_type: "infosource", right_image_number: 1 },
+    { choice_type: "free", left_image_type: "infosource", left_image_number: 1, right_image_type: "infosource", right_image_number: 0 },
+    { choice_type: "free", left_image_type: "infosource", left_image_number: 0, right_image_type: "infosource", right_image_number: 1 },
+    { choice_type: "free", left_image_type: "infosource", left_image_number: 1, right_image_type: "infosource", right_image_number: 0 },
+    { choice_type: "free", left_image_type: "infosource", left_image_number: 0, right_image_type: "infosource", right_image_number: 1 },
+    { choice_type: "free", left_image_type: "infosource", left_image_number: 1, right_image_type: "infosource", right_image_number: 0 },
+]
+
+var other_conditioning_stimuli = [
+
+    // free choice trials
+    {
+        choice_type: "free", left_info_image_type: "infosource", left_info_image_number: 0, right_info_image_type: "infosource", right_info_image_number: 1, left_bug_image_type: "bugmush", left_bug_image_number: 0, right_bug_image_type: "bugmush", right_bug_image_number: 1, InfoAnswerNumber: 0, numberq: 1
+    },
+    {
+        choice_type: "free", left_info_image_type: "infosource", left_info_image_number: 1, right_info_image_type: "infosource", right_info_image_number: 0, left_bug_image_type: "bugmush", left_bug_image_number: 1, right_bug_image_type: "bugmush", right_bug_image_number: 0, InfoAnswerNumber: 1, numberq: 2
+    },
+    {
+        choice_type: "free", left_info_image_type: "infosource", left_info_image_number: 0, right_info_image_type: "infosource", right_info_image_number: 1, left_bug_image_type: "bugmush", left_bug_image_number: 0, right_bug_image_type: "bugmush", right_bug_image_number: 1, InfoAnswerNumber: 1, numberq: 3
+    },
+    {
+        choice_type: "free", left_info_image_type: "infosource", left_info_image_number: 1, right_info_image_type: "infosource", right_info_image_number: 0, left_bug_image_type: "bugmush", left_bug_image_number: 1, right_bug_image_type: "bugmush", right_bug_image_number: 0, InfoAnswerNumber: 0, numberq: 4
+    },
+    {
+        choice_type: "free", left_info_image_type: "infosource", left_info_image_number: 0, right_info_image_type: "infosource", right_info_image_number: 1, left_bug_image_type: "bugmush", left_bug_image_number: 0, right_bug_image_type: "bugmush", right_bug_image_number: 1, InfoAnswerNumber: 1, numberq: 5
+    },
+    {
+        choice_type: "free", left_info_image_type: "infosource", left_info_image_number: 1, right_info_image_type: "infosource", right_info_image_number: 0, left_bug_image_type: "bugmush", left_bug_image_number: 1, right_bug_image_type: "bugmush", right_bug_image_number: 0, InfoAnswerNumber: 0, numberq: 6
+    },
+
+
+    //    // forced choice trials
+    //{
+    //    choice_type: "forced", left_info_image_type: "infosource", left_info_image_number: null, right_info_image_type: null, right_info_image_number: null, left_bug_image_type: "bugmush", left_bug_image_number: null, right_bug_image_type: null, right_bug_image_number: null, InfoAnswerNumber: 1, numberq: 7
+    //},
+    //{
+    //    choice_type: "forced", left_info_image_type: null, left_info_image_number: null, right_info_image_type: "infosource", right_info_image_number: null, left_bug_image_type: null, left_bug_image_number: null, right_bug_image_type: "bugmush", right_bug_image_number: null, InfoAnswerNumber: 0, numberq: 8
+    //},
+    //{
+    //    choice_type: "forced", left_info_image_type: "infosource", left_info_image_number: null, right_info_image_type: null, right_info_image_number: null, left_bug_image_type: "bugmush", left_bug_image_number: null, right_bug_image_type: null, right_bug_image_number: null, InfoAnswerNumber: 0, numberq: 9
+    //},
+    //{
+    //    choice_type: "forced", left_info_image_type: null, left_info_image_number: null, right_info_image_type: "infosource", right_info_image_number: null, left_bug_image_type: "bugmush", left_bug_image_number: null, right_bug_image_type: null, right_bug_image_number: null, InfoAnswerNumber: 1, numberq: 10
+    //}
+
+
+]
---
+++ js/bandit-stimuli.js
@@ -0,0 +1,47 @@
+// specify the images corresponding to simple cues
+var face_images = [
+  './img/f1.png',
+  './img/f2.png',
+  './img/f3.png',
+  './img/f4.png'
+]
+
+var place_images = [
+  './img/p1.png',
+  './img/p2.png',
+  './img/p3.png',
+  './img/p4.png'
+]
+
+// specify the different feedback images
+//var feedback_images = [
+//  './img/non-reward.png',
+//  './img/reward.png',
+//]
+var feedback_images = [
+  './img/Loss.jpg',//0
+  './img/Win.jpg',//1
+  './img/Star.jpg',//2
+  './img/Loss0.jpg',//3
+  './img/LossFalse.jpg',//4
+  './img/win1.jpg',//5
+  './img/wintrue.jpg',//6
+  './img/As.jpg',//7
+  './img/Bs.jpg',//8
+  './img/Ls.jpg',//9
+  './img/Ms.jpg'//10
+
+]
+
+var bugmush_images = [
+  './img/L.jpg',
+  './img/M.jpg',
+]
+
+var infosource_images = [
+  './img/A.jpg',
+  './img/B.jpg',
+
+]
+
+// specify the different feedback images
